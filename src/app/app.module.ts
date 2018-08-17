59e5befaa877006d09e8d22fbe9cd7bc35258261
jldaniel <jdanielae@gmail.com>
2018-08-16 18:17:06 -0700

    Added new system for and logic for adding an arbitrary number of variables.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Components
 import { AppComponent } from './app.component';
@@ -25,7 +26,9 @@ import { SystemDetailComponent } from './system-detail/system-detail.component';
   imports: [
     AppRoutingModule,
     BrowserModule,
-    HttpClientModule
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [DaedalusService],
   bootstrap: [AppComponent],
--- src/app/new-system/new-system.component.ts
+++ src/app/new-system/new-system.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, FormControl, FormArray} from '@angular/forms';
+
+import {InputVariable} from '../services/daedalus/InputVariable';
+import {OutputVariable} from '../services/daedalus/OutputVariable';
+import {System} from '../services/daedalus/System';
 
 @Component({
   selector: 'app-new-system',
@@ -7,9 +12,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewSystemComponent implements OnInit {
 
-  constructor() { }
+  system: System;
+  systemForm: FormGroup;
+  inputVariables: FormArray;
+  outputVariables: FormArray;
 
-  ngOnInit() {
+
+  constructor(private formBuilder: FormBuilder) {
   }
 
+  ngOnInit() {
+
+    this.systemForm = this.formBuilder.group({
+      name: new FormControl(''),
+      description: new FormControl(''),
+      inputVariables: this.formBuilder.array([this.createInputVariable()]),
+      outputVariables: this.formBuilder.array([this.createOutputVariable()])
+    });
+
+  }
+
+  createInputVariable(): FormGroup {
+    return this.formBuilder.group({
+      name: ''
+    });
+  }
+
+  createOutputVariable(): FormGroup {
+    return this.formBuilder.group({
+      name: ''
+    });
+  }
+
+  addInputVariable(): void {
+    this.inputVariables = this.systemForm.get('inputVariables') as FormArray;
+    this.inputVariables.push(this.createInputVariable());
+  }
+
+  addOutputVariable() {
+    this.outputVariables = this.systemForm.get('outputVariables') as FormArray;
+    this.outputVariables.push(this.createOutputVariable());
+  }
+
+  onSubmit() {
+    console.log('new system form submitted');
+    console.log(this.systemForm.get('name').value);
+    console.log(this.systemForm.get('description').value);
+
+    /* TODO Fix type errors */
+    const outputVarsGroup: FormArray = this.systemForm.get('intputVariables');
+    const outputVars: FormArray = outputVarsGroup.controls;
+
+    for (let input of this.systemForm.get('inputVariables').controls as FormArray) {
+      console.log(input.get('name').value);
+    }
+
+    for (let output of this.systemForm.get('outputVariables').controls as FormArray) {
+      console.log(output.get('name').value);
+    }
+
+  }
 }
